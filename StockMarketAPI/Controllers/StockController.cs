cb6a7d3321dfe9b26c6e06903848051aa342e62a
akcinar.omer <akcinardev@gmail.com>
2024-07-25 20:45:41 +0200

    Added StockController

---
+++ StockMarketAPI/Controllers/StockController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockMarketAPI.Data;
+
+namespace StockMarketAPI.Controllers
+{
+	[Route("api/stock")]
+	[ApiController]
+	public class StockController : ControllerBase
+	{
+        private readonly ApplicationDbContext _context;
+
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
+    }
+}
