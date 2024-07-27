6c1761bf0119ee84d714f7817b9ab2413392c2ca
akcinar.omer <akcinardev@gmail.com>
2024-07-27 15:57:22 +0200

    Delete comment operation implemented

--- StockMarketAPI/Interfaces/ICommentRepository.cs
+++ StockMarketAPI/Interfaces/ICommentRepository.cs
@@ -8,5 +8,6 @@ namespace StockMarketAPI.Interfaces
 		Task<Comment?> GetByIdAsync(int id);
 		Task<Comment> CreateAsync(Comment commentModel);
 		Task<Comment?> UpdateAsync(int id, Comment commentModel);
+		Task<Comment?> DeleteAsync(int id);
 	}
 }
